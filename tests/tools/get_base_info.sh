c608afa72d9e42de94559838d14cc5e35d90efc7
Lht97 <lhtian97@gmail.com>
2026-02-22 00:47:05 +0800

    add a safeguade to make sure the form of artifact is zip

--- tests/tools/get_base_info.sh
+++ tests/tools/get_base_info.sh
@@ -1,2 +1,4 @@
+# Find the target directory or file
 ARTIFACT_NAME=$(ls | grep "profile_optiprofiler" | head -n 1)
-BASE_INFO=$(echo $ARTIFACT_NAME | sed -n 's/.*optiprofiler_\(.*_\(small\|big\|large\)\).*/\1/p')
+# Extract the base information and remove any carriage returns or newlines
+BASE_INFO=$(echo "$ARTIFACT_NAME" | sed -n 's/.*optiprofiler_\(.*_\(small\|big\|large\)\).*/\1/p' | tr -d '\r\n')
